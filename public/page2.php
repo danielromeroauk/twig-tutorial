d7c347d84dde7fe7a86dc13e0c91241a59b9e3f7
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2016-07-23 04:39:39 -0500

    Se creó el archivo .editorconfig Cambiada la indentación de 2 a 4 espacios en page2.

--- public/page2.php
+++ public/page2.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . '/../config.php';
 
 $people = [
-  'Daniel',
-  'Juan',
-  'Pedro',
-  'Carlos'
+    'Daniel',
+    'Juan',
+    'Pedro',
+    'Carlos'
 ];
 
 echo $twig->render('view3.twig', compact('people'));
